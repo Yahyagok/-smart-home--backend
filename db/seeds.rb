a6298fba752cb3fc73c7953370f965b766cacca9
Yahyagok <ygok17@hotmail.com>
2020-05-26 15:44:42 -0400

    backend side

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.destroy_all
+home = Home.destroy_all
+utility = Utility.destroy_all 
+
+user1 = User.create(name: "Yahya")
+user2 = User.create(name: "Carly")
+
+home1 = Home.create(name: "Walnut")
+home2 =  Home.create(name: "pistachu")
+
+utility1 = Utility.create(home_id: home1.id , user_id: user1.id, washing: false,  door: false, temperature: rand(40..70), light: false, alarm_system: true)
+
+utility2 =  Utility.create(home_id: home1.id , user_id: user1.id, washing: false, door: true, temperature: rand(40..70), light: true, alarm_system: false)
+
+
